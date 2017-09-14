712aa9e6cca89d57d9a3b7eb7a287d40b73826fb
BerraBelin96 <berrabelin96@gmail.com>
2017-09-14 10:37:46 +0200

    La till fil

---
+++ slackbot.php
@@ -0,0 +1,153 @@
+<?php
+
+require 'logins.php';
+
+
+$cmText = filter_input(INPUT_POST, "text", FILTER_SANITIZE_SPECIAL_CHARS);
+//$cmText = $_POST["text"];
+
+$line1 = 'Line 1';
+$line2 = 'Line 2';
+/*$i = 1;
+while ($i <= 10) {
+	'\n'.$line1.
+	$i = $i + 1;
+}*/
+
+/*$data = '
+{
+    "response_type": "ephemeral",
+    "text": "In list:",
+    "attachments": [
+        {
+            "text":"'.'\n'.$line1.'\n'.$line2.'"
+        }
+    ]
+} 
+';*/
+
+if(isset($cmText)){
+	header('Content-type:application/json;charset=utf-8');
+	
+	if (substr($cmText, 0, strlen('add ')) === 'add ') {
+	    
+	    //echo json_encode(" Found add: " . $cmText);
+	    
+	    $cmText = substr($cmText, strlen('add '));
+
+	    if(!empty($cmText)){
+	    	http_response_code(200);
+	    	echo json_encode("Added: " . $cmText, JSON_UNESCAPED_UNICODE);
+
+
+	    	$mysqli = new mysqli("$host", "$user", "$pass", "$database");
+
+			$fråga = "test";
+			$typ = "";
+
+		    $filtered = $cmText;
+		    //echo $cmText;
+			$filtered = trim($filtered);
+
+			if (!$mysqli->set_charset("utf8")) {
+			    echo "Error loading character set utf8: " . $mysqli->error;
+			    exit();
+			}
+
+		    if (!($stmt = $mysqli->prepare("INSERT into frågor(fraga, typ) values (?,?)"))) {
+				echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			}
+
+			if (!$stmt->bind_param("si", $filtered, $typ)) {
+				echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+
+			if (!$stmt->execute()) {
+			    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+
+			$filtered = $mysqli->real_escape_string($filtered);
+
+		    $mysqli->close();
+
+
+		}
+		else{
+			http_response_code(200);
+			echo json_encode("Blank", JSON_UNESCAPED_UNICODE);
+		}
+	} elseif (substr($cmText, 0, strlen('list')) === 'list') {
+	    //header('Content-type:application/json;charset=utf-8');
+	    
+	    //echo json_encode("Found list: " . $cmText);
+	    
+	    //$cmText = substr($cmText, strlen('list '));
+		//http_response_code(200);
+		
+		/*$json_array = array();
+		$i = 1;
+		while ($i <= 10) {
+			$json_array[] = '\n'.$i;
+			$i = $i + 1;
+		}*/
+		echo '
+{
+    "response_type": "ephemeral",
+    "text": "In list:",
+    "attachments": [
+        {
+            "text":"';
+
+            	$i = 1;
+				while ($i <= 10) { 
+					echo '\n'.$i; 
+					$i = $i + 1; 
+				}
+
+            echo'"
+        }
+    ]
+} 
+';
+    	
+
+
+		//$json_array[] = $row;
+
+
+
+		/*$i = 1;
+	    echo '
+			{
+			    "response_type": "ephemeral",
+			    "text": "In list:",
+			    "attachments": [
+			        {
+			            "text":"'. $i = 1;
+						while ($i <= 10) { echo '\n'.$i.''; $i = $i + 1; } "".
+						 '"
+			        }
+			    ]
+			} 
+			';*/
+
+	    /*if(!empty($cmText)){
+	    	echo json_encode("Removed list: " . $cmText, JSON_UNESCAPED_UNICODE);
+		}
+		else{
+			echo json_encode("Blank", JSON_UNESCAPED_UNICODE);
+		}*/
+	} else {
+		http_response_code(200);
+	    echo json_encode("Non specific command", JSON_UNESCAPED_UNICODE);
+
+	    
+	}
+} 
+else{
+	http_response_code(400);
+	header('Content-type:application/json;charset=utf-8');
+	echo json_encode("Invalid syntax", JSON_UNESCAPED_UNICODE);
+}
+
+?>
