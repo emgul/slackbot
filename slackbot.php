c7ed2bcba2f6c2bc33598b70bdac841079778504
BerraBelin96 <berrabelin96@gmail.com>
2017-09-18 13:41:18 +0200

    Revert "Merge branch 'master' of https://github.com/emgul/slackbot"
    This reverts commit 80b1c72c136fd84e5428537e6bb7c5de56bb1e8b, reversing
    changes made to 633e413e7aa3bcc9d0b9bcf28b4b4dee320efa8e.

--- slackbot.php
+++ slackbot.php
@@ -7,8 +7,8 @@ $cmText = filter_input(INPUT_POST, "text", FILTER_SANITIZE_SPECIAL_CHARS);
 //echo $cmText;
 //$cmText = $_POST["text"];
 
-//$line1 = 'Line 1';
-//$line2 = 'Line 2';
+$line1 = 'Line 1';
+$line2 = 'Line 2';
 /*$i = 1;
 while ($i <= 10) {
 	'\n'.$line1.
@@ -36,9 +36,11 @@ if(isset($cmText)){
 	    
 	    $cmText = substr($cmText, strlen('add '));
 
-	    $cmText = trim($cmText);
-	    
 	    if(!empty($cmText)){
+	    	http_response_code(200);
+	    	echo json_encode("Added: " . $cmText, JSON_UNESCAPED_UNICODE);
+
+
 	    	$mysqli = new mysqli("$host", "$user", "$pass", "$database");
 
 			$fråga = "test";
@@ -71,8 +73,6 @@ if(isset($cmText)){
 
 		    $mysqli->close();
 
-		    http_response_code(200);
-	    	echo json_encode("Added: " . $cmText, JSON_UNESCAPED_UNICODE);
 
 		}
 		else{
@@ -94,21 +94,20 @@ if(isset($cmText)){
 			$i = $i + 1;
 		}*/
 		echo '
-			{
-			    "response_type": "in_channel",
-			    "text": "In list:",
-			    "attachments": [
-			        {
-			            "text":"';
+{
+    "response_type": "ephemeral",
+    "text": "In list:",
+    "attachments": [
+        {
+            "text":"';
 
-			            	$mysqli = new mysqli("$host", "$user", "$pass", "$database");
+            	$mysqli = new mysqli("$host", "$user", "$pass", "$database");
 
-			            	if (!$mysqli->set_charset("utf8")) {
-							    echo "Error loading character set utf8: " . $mysqli->error;
-							    exit();
-							}
+            	if (!$mysqli->set_charset("utf8")) {
+				    echo "Error loading character set utf8: " . $mysqli->error;
+				    exit();
+				}
 
-<<<<<<< HEAD
 				$result = $mysqli->query("SELECT * FROM questions");
 				if(mysqli_num_rows($result) > 0)
 				{
@@ -120,26 +119,14 @@ if(isset($cmText)){
 				else{
 				    echo "List is empty";
 				}
-=======
-							$result = $mysqli->query("SELECT * FROM questions");
-							if(mysqli_num_rows($result) > 0)
-							{
-							    while ($row = $result->fetch_assoc()) {
-							        echo '\n' . $row["fraga"];
-							    }
-							}
-							else{
-							    echo "List is empty";
-							}
->>>>>>> 02e23814d248e40479e424a59ea3f6c7e26b021b
 
-							$mysqli->close();
+				$mysqli->close();
 
-			            echo'"
-			        }
-			    ]
-			} 
-			';
+            echo'"
+        }
+    ]
+} 
+';
     	
 
 
